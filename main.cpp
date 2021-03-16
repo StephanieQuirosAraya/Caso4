18fe50e255041188e743e31268f2c986c54c63b3
Pamela Dinarte <p.dinartech@gmail.com>
2021-03-16 17:16:45 -0600

    Pattern mejorado

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 # include "pattern.cpp"
 
 int main(){
-    pattern(60,120);
+    pattern(70,120);
     
     return 0;
 }
--- pattern.cpp
+++ pattern.cpp
@@ -6,89 +6,47 @@
 using namespace std;
 
 void pattern(double alto, double ancho){
-    double radius;
-    double radiusO;
-    double desplAlto;
-    double desplAncho;
-    double x, y;
-    int suma = alto/4;      //para sumarle a los rangos
+    int radio;               //radio actual del circulo
+    int desplazamX;           //desplazamiento para que se vean bonitos los circulos
+    int desplazamY;
+    double x, y;             //coordenadas de cada punto creado
+    int aumento = 0;         //aumento de las circunferencias
+    int suma = alto/4;       //para sumarle a los rangos
     int rango1I = 0;         //rangos de las filas de cada cuarto (para partir los círculos)
     int rango1F = suma;
     int rango2I = rango1F + suma;
     int rango2F = rango2I + suma;
     vector <vector<char> > circulo(alto, vector<char>(ancho,'-'));
 
-    /*
-    if(alto > ancho){
-        radius = alto/2; 
-    }
-    else{
-        radius = ancho/2;
-    }
-    
-    radiusO = radius;
-    radius = alto/2;
-    radius += 5;
-    for(int reductor = 0;reductor <= radius-10; reductor+=5){
-        radius -= 5;
-        for(double t = 0.0; t < (6.3)*10; t += 0.1){
-            x = round(radius * sin(t) + radius);
-            y = round(radius * cos(t) + radius + 1);
-            if(x+reductor<alto && y+reductor<ancho){
-                circulo[x+reductor][y+reductor] = '*';
-            }
+    desplazamY = alto*0.375;    //-10 para que haga dos circulos mas al centro
+    desplazamX = ancho/3;
+    radio = desplazamY - 20;     //radio inicial
 
-        }
-    }
-    */
-    radius = alto*0.375;  //el +5 es por el comienzo del ciclo, que se resta 5, entonces para que el radio quede bien de igual forma
-    radiusO = radius;
-    radius += 5;
-    for(int reductor = 0;reductor <= radius; reductor+=5){ //reductor <= radius-10
-        radius -= 5;
-        for(double t = 0.0; t < (6.3)*10; t += 0.1){
-            x = round(radius * sin(t) + radius);
-            y = round(radius * cos(t) + radius);
-            if(x+reductor<alto && y+reductor<ancho){
-                if(x+reductor >= rango1I && x+reductor < rango1F ||
-                   x+reductor >= rango2I && x+reductor < rango2F){
-                    circulo[x+reductor][y+reductor] = '*';
+    for( ; aumento <= ancho-desplazamX ; aumento+=5 ){
+        for(double angulo = 0.0 ; angulo < 6.3*10 ; angulo+=0.1 ){
+            x = round(radio * cos(angulo))  + desplazamX;
+            y = round(radio * sin(angulo))  + desplazamY;
+            if(  y < alto && y >= 0 && x < ancho && x >= 0  ){
+                if(  y >= rango1I && y < rango1F || y >= rango2I && y < rango2F  ){
+                    circulo[y][x] = '*';
+                }
+                if(  (y+suma >= rango1I+suma && y+suma < rango1F+suma) || (y+suma >= rango2I+suma && y+suma < alto)  ){
+                    y += suma;
+                    x += (ancho/3);
+                    if( x >= 0 && x < ancho )
+                        circulo[y][x] = '*';
                 }
             }
         }
-    }
-    radius = radiusO;
-    rango1I += suma;
-    rango1F += suma;
-    rango2I += suma;
-    rango2F += suma;
-    desplAlto = alto * 0.25;
-    desplAncho = ancho/2 - radius;
-    radius += 5;
-    for(int reductor = 0;reductor <= radius; reductor+=5){ //reductor <= radius-10
-        radius -= 5;
-        for(double t = 0.0; t < (6.3)*10; t += 0.1){
-            x = round(radius * sin(t) + radius) + desplAlto;
-            y = round(radius * cos(t) + radius) + desplAncho;
-            if(x+reductor<alto && y+reductor<ancho){
-                if(x+reductor >= rango1I && x+reductor < rango1F ||
-                   x+reductor >= rango2I && x+reductor < rango2F){
-                    circulo[x+reductor][y+reductor] = '*';
-                }
-            }
-        }
+        radio += 5;
     }
 
-    
-    
 
-
-
-    for(const auto& row : circulo){
-        string line;
-        for(auto cell : row){
-            line += cell;
+    for(const auto& fila : circulo){    //itera las filas de la matriz
+        string linea;                   
+        for(auto caracter : fila){      //va uniendo los caracteres de la fila en un solo string
+            linea += caracter;    
         }
-        cout << line << endl;
+        cout << linea << endl;          //imprime una fila
     }
 }
